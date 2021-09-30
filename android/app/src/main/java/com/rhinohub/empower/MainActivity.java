ea79951821fb8646b629112daa82879e69b7da65
musera <irakli.museridze@ol.ge>
2021-09-30 20:15:13 +0400

    commit

--- android/app/src/main/java/com/rhinohub/empower/MainActivity.java
+++ android/app/src/main/java/com/rhinohub/empower/MainActivity.java
@@ -1,4 +1,4 @@
-package com.socializer;
+package com.rhinohub.empower;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Socializer";
+    return "Empower";
   }
 
   @Override
--- ios/EmpowerTests/EmpowerTests.m
+++ ios/EmpowerTests/EmpowerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SocializerTests : XCTestCase
+@interface EmpowerTests : XCTestCase
 
 @end
 
-@implementation SocializerTests
+@implementation EmpowerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
