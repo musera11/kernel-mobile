fe1f0860f4e55258b9d06617a13b726de59fe5f6
musera <irakli.museridze@ol.ge>
2021-10-18 15:16:58 +0400

    commit

--- src/store/storeRegistry.ts
+++ src/store/storeRegistry.ts
@@ -3,7 +3,6 @@ import {Store} from 'redux';
 let reduxStore: Store;
 export default {
   register: (store: Store) => {
-    console.log(store.getState());
     reduxStore = store;
   },
   getStore: () => reduxStore,
